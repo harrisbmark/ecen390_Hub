ad316a6958a29684c019dc8787e31428cafa2a0a
Bryan Mark Harris <mark@harrisbmark.com>
2017-04-11 04:55:01 -0600

    Changed CREATE OR REPLACE to just CREATE.

--- lasertag.sql
+++ lasertag.sql
@@ -1,29 +1,29 @@
 -- Create the shots table
-CREATE OR REPLACE TABLE shots (player_frequency SERIAL NOT NULL PRIMARY KEY, shots integer);
+CREATE TABLE shots (player_frequency SERIAL NOT NULL PRIMARY KEY, shots integer);
 
 -- Create all the schemas for the different players (used for the hit tables)
-CREATE OR REPLACE SCHEMA player1;
-CREATE OR REPLACE SCHEMA player2;
-CREATE OR REPLACE SCHEMA player3;
-CREATE OR REPLACE SCHEMA player4;
-CREATE OR REPLACE SCHEMA player5;
-CREATE OR REPLACE SCHEMA player6;
-CREATE OR REPLACE SCHEMA player7;
-CREATE OR REPLACE SCHEMA player8;
-CREATE OR REPLACE SCHEMA player9;
-CREATE OR REPLACE SCHEMA player10;
+CREATE SCHEMA player1;
+CREATE SCHEMA player2;
+CREATE SCHEMA player3;
+CREATE SCHEMA player4;
+CREATE SCHEMA player5;
+CREATE SCHEMA player6;
+CREATE SCHEMA player7;
+CREATE SCHEMA player8;
+CREATE SCHEMA player9;
+CREATE SCHEMA player10;
 
 -- Create all the hit tables for each player
-CREATE OR REPLACE TABLE player1.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player2.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player3.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player4.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player5.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player6.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player7.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player8.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player9.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
-CREATE OR REPLACE TABLE player10.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player1.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player2.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player3.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player4.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player5.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player6.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player7.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player8.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player9.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
+CREATE TABLE player10.hits (player_frequency SERIAL NOT NULL PRIMARY KEY, hits integer);
 
 -- Insert 10 values into the shots table
 INSERT INTO shots (shots) VALUES (0);
@@ -149,7 +149,7 @@ INSERT INTO player10.hits (hits) VALUES (0);
 INSERT INTO player10.hits (hits) VALUES (0);
 
 -- Create a view to show the total takedowns for each player
-CREATE OR REPLACE VIEW takedowns AS SELECT (SELECT hits FROM player1.hits WHERE player_frequency=1) +
+CREATE VIEW takedowns AS SELECT (SELECT hits FROM player1.hits WHERE player_frequency=1) +
        (SELECT hits FROM player2.hits WHERE player_frequency=1) +
        (SELECT hits FROM player3.hits WHERE player_frequency=1) +
        (SELECT hits FROM player4.hits WHERE player_frequency=1) +
@@ -251,7 +251,7 @@ CREATE OR REPLACE VIEW takedowns AS SELECT (SELECT hits FROM player1.hits WHERE 
        (SELECT hits FROM player10.hits WHERE player_frequency=1) AS player_10;
 
 -- Create a view to show total hits for each player.
-CREATE OR REPLACE VIEW hits AS SELECT (SELECT hits FROM player1.hits WHERE player_frequency=1) +
+CREATE VIEW hits AS SELECT (SELECT hits FROM player1.hits WHERE player_frequency=1) +
        (SELECT hits FROM player1.hits WHERE player_frequency=2) +
        (SELECT hits FROM player1.hits WHERE player_frequency=3) +
        (SELECT hits FROM player1.hits WHERE player_frequency=4) +
@@ -353,19 +353,19 @@ CREATE OR REPLACE VIEW hits AS SELECT (SELECT hits FROM player1.hits WHERE playe
        (SELECT hits FROM player10.hits WHERE player_frequency=10) AS player_10;
 
 -- Create function to return the takedown value for a player
-CREATE OR REPLACE FUNCTION get_takedown_value(player_frequency integer) RETURNS TABLE (fc json) AS $$ BEGIN IF player_frequency = 1 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_1) FROM takedowns; ELSIF player_frequency = 2 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_2) FROM takedowns; ELSIF player_frequency = 3 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_3) FROM takedowns; ELSIF player_frequency = 4 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_4) FROM takedowns; ELSIF player_frequency = 5 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_5) FROM takedowns; ELSIF player_frequency = 6 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_6) FROM takedowns; ELSIF player_frequency = 7 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_7) FROM takedowns; ELSIF player_frequency = 8 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_8) FROM takedowns; ELSIF player_frequency = 9 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_9) FROM takedowns; ELSIF player_frequency = 10 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_10) FROM takedowns; ELSE RETURN QUERY SELECT json_build_object('player_frquency', 0, 'takedowns', 0) FROM takedowns; END IF; RETURN; END; $$ LANGUAGE plpgsql;
+CREATE FUNCTION get_takedown_value(player_frequency integer) RETURNS TABLE (fc json) AS $$ BEGIN IF player_frequency = 1 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_1) FROM takedowns; ELSIF player_frequency = 2 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_2) FROM takedowns; ELSIF player_frequency = 3 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_3) FROM takedowns; ELSIF player_frequency = 4 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_4) FROM takedowns; ELSIF player_frequency = 5 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_5) FROM takedowns; ELSIF player_frequency = 6 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_6) FROM takedowns; ELSIF player_frequency = 7 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_7) FROM takedowns; ELSIF player_frequency = 8 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_8) FROM takedowns; ELSIF player_frequency = 9 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_9) FROM takedowns; ELSIF player_frequency = 10 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'takedowns', player_10) FROM takedowns; ELSE RETURN QUERY SELECT json_build_object('player_frquency', 0, 'takedowns', 0) FROM takedowns; END IF; RETURN; END; $$ LANGUAGE plpgsql;
 
 -- Create function to return the hits value for a player
-CREATE OR REPLACE FUNCTION get_hit_value(player_frequency integer) RETURNS TABLE (fc json) AS $$ BEGIN IF player_frequency = 1 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_1) FROM hits; ELSIF player_frequency = 2 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_2) FROM hits; ELSIF player_frequency = 3 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_3) FROM hits; ELSIF player_frequency = 4 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_4) FROM hits; ELSIF player_frequency = 5 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_5) FROM hits; ELSIF player_frequency = 6 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_6) FROM hits; ELSIF player_frequency = 7 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_7) FROM hits; ELSIF player_frequency = 8 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_8) FROM hits; ELSIF player_frequency = 9 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_9) FROM hits; ELSIF player_frequency = 10 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_10) FROM hits; ELSE RETURN QUERY SELECT json_build_object('player_frquency', 0, 'hits', 0) FROM hits; END IF; RETURN; END; $$ LANGUAGE plpgsql;
+CREATE FUNCTION get_hit_value(player_frequency integer) RETURNS TABLE (fc json) AS $$ BEGIN IF player_frequency = 1 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_1) FROM hits; ELSIF player_frequency = 2 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_2) FROM hits; ELSIF player_frequency = 3 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_3) FROM hits; ELSIF player_frequency = 4 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_4) FROM hits; ELSIF player_frequency = 5 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_5) FROM hits; ELSIF player_frequency = 6 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_6) FROM hits; ELSIF player_frequency = 7 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_7) FROM hits; ELSIF player_frequency = 8 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_8) FROM hits; ELSIF player_frequency = 9 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_9) FROM hits; ELSIF player_frequency = 10 THEN RETURN QUERY SELECT json_build_object('player_frquency', player_frequency, 'hits', player_10) FROM hits; ELSE RETURN QUERY SELECT json_build_object('player_frquency', 0, 'hits', 0) FROM hits; END IF; RETURN; END; $$ LANGUAGE plpgsql;
 
 -- Create the function to be used by the triggers on tables (announces a 'watcher' for NodeJS socekt)
-CREATE OR REPLACE FUNCTION notify_table_trigger() RETURNS trigger AS $$ DECLARE BEGIN PERFORM pg_notify('watchers', jsonb_merge(get_takedown_value(player_frequency), get_hit_value(player_frequency))); RETURN NULL; END; $$ LANGUAGE plpgsql;
+CREATE FUNCTION notify_table_trigger() RETURNS trigger AS $$ DECLARE BEGIN PERFORM pg_notify('watchers', jsonb_merge(get_takedown_value(player_frequency), get_hit_value(player_frequency))); RETURN NULL; END; $$ LANGUAGE plpgsql;
 
 -- Create the function to be used by the triggers on the takedown view (announces a 'watcher' for NodeJS socekt)
---CREATE OR REPLACE FUNCTION notify_takedowns_view_trigger() RETURNS trigger AS $$ DECLARE BEGIN PERFORM pg_notify('watchers', get_takedown_value(NEW.player_frequency)::text); RETURN new; END; $$ LANGUAGE plpgsql;
+--CREATE FUNCTION notify_takedowns_view_trigger() RETURNS trigger AS $$ DECLARE BEGIN PERFORM pg_notify('watchers', get_takedown_value(NEW.player_frequency)::text); RETURN new; END; $$ LANGUAGE plpgsql;
 
 -- Create the function to be used by the triggers on the hits view (announces a 'watcher' for NodeJS socekt)
---CREATE OR REPLACE FUNCTION notify_hits_view_trigger() RETURNS trigger AS $$ DECLARE BEGIN PERFORM pg_notify('watchers', get_hit_value(NEW.player_frequency)::text); RETURN new; END; $$ LANGUAGE plpgsql;
+--CREATE FUNCTION notify_hits_view_trigger() RETURNS trigger AS $$ DECLARE BEGIN PERFORM pg_notify('watchers', get_hit_value(NEW.player_frequency)::text); RETURN new; END; $$ LANGUAGE plpgsql;
 
 -- Create trigger for the shots table
 --CREATE TRIGGER shots_table_trigger AFTER UPDATE ON shots FOR EACH ROW EXECUTE PROCEDURE notify_table_trigger();
